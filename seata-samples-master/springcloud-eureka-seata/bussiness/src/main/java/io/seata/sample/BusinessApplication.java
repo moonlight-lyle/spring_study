b1966904a60fa4ea3618bb92fc900573f07debb1
moonlight-lyle <15755589769@163.com>
2021-10-10 11:31:16 +0800

    Seata的使用案例

---
+++ seata-samples-master/springcloud-eureka-seata/bussiness/src/main/java/io/seata/sample/BusinessApplication.java
@@ -0,0 +1,35 @@
+package io.seata.sample;
+
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author jimin.jm@alibaba-inc.com
+ * @date 2019/06/14
+ */
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class,DruidDataSourceAutoConfigure.class}) // 该微服务不需要数据源，排除掉
+@EnableFeignClients
+@EnableEurekaClient
+public class BusinessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessApplication.class, args);
+    }
+
+   /* @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        DruidDataSource druidDataSource = new DruidDataSource();
+        return druidDataSource;
+    }
+
+    @Bean("jdbcTemplate")
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }*/
+
+}
